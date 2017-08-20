c32f7e4340d6d335b49ecf5a5840c8086ea4a57d
Jacob Grosner <JacobGrosner@Jacobs-MBP.home>
2017-08-20 11:17:12 -0400

    title func

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import xlsxwriter
+import re
 from urllib.request import Request, urlopen
 
 list = [1,2,3,4,5,6,7,8,9]
@@ -39,20 +40,63 @@ list = list[:len(list)-(c-1)]
 
 list[len(list)-1] = list[len(list)-1][:1300]
 
+#print (list[0])
+
+'"<a href="/game/playstation-4/aca-neogeo-metal-slug-2">"'  
+regex = r"([<][a][ ][h][r][e][f](.*)[n]([ ]){28}[\w])"
+#mo = re.search(regex, list[0])
+#print (mo) 
+#print()
+#print (mo.group())
+#print (mo.start())
+#print (mo.end())
+#cutOff = mo.end()-1
+#list[0] = list[0][cutOff:]
+#print (list[0])
 
 
 x = 0
-for l in list: 
-	print ("item")
-	print(x)
-	print(l) 
-	print()
-	x = x+1
+#for l in list: 
+#	print ("item")
+#	print(x)
+#	print(l) 
+#	print()
+#	x = x+1
 
 print (len(list))
 
 
 #myfile = f.read()
 #print(webpage)
+def findTitle(str):
+	goal = ""
+	for c in str:
+		if c != "\\":
+			goal=goal+c
+		else:
+			break
+	return goal 
+
+print(findTitle(list[0]))
+
+#goal: make list of titles 
+
+
+for l in list:
+	mo = re.search(regex, l)
+	#print (mo)
+	cutOff = mo.end()-1
+	l = l[cutOff:]
+	print(findTitle(l)) #dope you found the titles! Add to tuple with score and date later 
+
+
+	
+
+	print()
+	
+
+
+
+
 
 
